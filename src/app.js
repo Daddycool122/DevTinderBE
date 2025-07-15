0bfc4ccbd58be30fbff412589a94e4e8218dbb3b
Daddycool122 <akhileshramola.2000@gmail.com>
2025-07-15 13:03:34 +0530

    Route handlers

--- src/app.js
+++ src/app.js
@@ -2,31 +2,53 @@ const express = require('express');
 
 const app = express();
 
-app.get("/",(req,res)=>{
-    res.send('Hello World');
-})
-
-
-app.get("/user",(req,res)=>{
-    res.send({"firstName":"John","lastName":"Doe"});
-})
-
-app.post("/user",(req,res)=>{
-    console.log("Received POST request to /user");
+// app.get( /ab+d/,(req,res)=>{
     
-    res.send('Data saved successfully to db');
-})
+//     res.send("Regular expression matched successfully");
+// });
 
-app.delete("/user",(req,res)=>{
+
+// app.get("/user/:userId/:name/:password",(req,res)=>{
+//     console.log(req.params)
+//     res.send({"firstName":"John","lastName":"Doe"});
+// })
+
+
+
+
+// app.post("/user",(req,res)=>{
+//     console.log("Received POST request to /user");
+    
+//     res.send('Data saved successfully to db');
+// })
+
+// app.delete("/user",(req,res)=>{
    
     
-    res.send('Data deleted successfully from the db');
-})
+//     res.send('Data deleted successfully from the db');
+// })
 
 
 
 
-
+app.use("/user",
+    [(req,res,next)=>{
+    console.log("User log");
+    next();
+    // res.send("Response 1 from user")
+    
+               },
+    (req,res,next)=>{
+    console.log("User log 2");
+    next();
+    
+    // res.send("Response 2 from user")
+               },
+    (req,res)=>{
+    console.log("User log 3");
+    res.send("Response 3 from user")
+               },]
+        )
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000');
