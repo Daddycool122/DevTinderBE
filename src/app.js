4a3ca3d1b84c6e9c25813e83d10b04c9acbcf1e6
Daddycool122 <akhileshramola.2000@gmail.com>
2025-07-19 16:51:58 +0530

    update-delete-get-apis

--- src/app.js
+++ src/app.js
@@ -7,24 +7,23 @@ const User = require('./models/user')
 
 app.use(express.json());
 
-
-app.post('/user',async(req,res)=>{
+// Create/Sign up a User
+// app.post('/user',async(req,res)=>{
     
-    const user = new User(req.body)
+//     const user = new User(req.body)
 
-    try{
-        await user.save();
-    res.send('User created successfully');
-    }
-    catch(err){
-        res.status(500).send('Error creating user: ' + err.message);
-    }
+//     try{
+//         await user.save();
+//     res.send('User created successfully');
+//     }
+//     catch(err){
+//         res.status(500).send('Error creating user: ' + err.message);
+//     }
     
-})
+// })
 
 
 // get user by email (find)
-
 // app.get('/user',async (req,res)=>{
 //     const userEmail = req.body.email;
 //     try{
@@ -63,20 +62,99 @@ app.post('/user',async(req,res)=>{
 
 
 // get user by id (findById)
-app.get('/user/:userId',async(req,res)=>{
-    const user = await User.findById(req.params.userId);
-    console.log(req.params);
+// app.get('/user',async(req,res)=>{
+//     const userId= req.body.userId;
+
+//     try{
+//         const user = await User.findById(userId);
+//         if(!user){
+//             return res.status(404).send('User not found');
+//         }
+//         else{
+//             res.send(user);
+//         }
+//     }
+//     catch(err){
+//         res.status(500).send('Error fetching user: ' + err.message);
+//     }
     
-    res.send(user);
-})
+    
+    
+// })
 
 
 // get all users
-app.get('/feed', async (req,res)=>{
-    const user = await User.find({});
-    res.send(user);
-})
+// app.get('/feed', async (req,res)=>{
+//     const user = await User.find({});
+//     res.send(user);
+// })
 
+
+// delete user api
+
+// app.delete('/user',async (req,res)=>{
+//     try{
+//         const userId = req.body.userId;
+//         const user = await User.findByIdAndDelete(userId);
+//         if(!user){
+//             res.send('User not found or already deleted');
+//         }
+//         else{
+//             res.send("User deleted successfully")
+//         }
+
+//     }
+//     catch(err){
+//         res.status(500).send('Error deleting user: ' + err.message);
+//     }
+// })
+
+
+// update api (by userId)
+
+// app.patch('/user',async (req,res)=>{
+//     try{
+//         const userId = req.body.userId;
+//         const user = await User.findByIdAndUpdate(userId,req.body,{returnDocument: 'after'});
+//         console.log(user);
+//         if(!user){
+//             res.status(400).send('No user fond to update')
+//         }
+//         else{
+//              res.send("User updated Successfully")
+//         }
+        
+        
+//     }
+//     catch(err){
+//         res.status(500).send('Error updating user: ' + err.message);
+//     }
+// })
+
+// update api (by email)
+// app.patch('/user',async(req,res)=>{
+//     try{
+//         const userEmail =  req.body.email;
+        
+//         const data = req.body;
+        
+//         const user = await User.findOneAndUpdate({email: userEmail},data);
+//         console.log(user);
+        
+//         if(!user){
+//            return res.status(404).send('No user found with this email');
+//         }
+//         else{
+//            return res.send("User updated successfully by email");
+//         }
+        
+        
+        
+//     }
+//     catch(err){
+//         res.status(500).send('Error updating user: ' + err.message);
+//     }
+// })
 
 connectDb().then(()=>{
     console.log('Database connected successfully');
